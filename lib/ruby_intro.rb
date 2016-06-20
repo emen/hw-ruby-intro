6762d36220a949199d6a2fbefb81e42cd3339ad5
Emen Zhao <emenzhaogq@gmail.com>
2016-06-19 22:07:12 -0700

    Add solution

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,46 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum( arr.inject([]) { |max_2, elem| (max_2 << elem).max(2) } )
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  (0...arr.length-1).any? do |i|
+    (i+1...arr.length).any? { |j| arr[i] + arr[j] == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match(/\A(?![aeiou])[a-z]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s.match(/\A[01]+\z/) && (s == '0' || s.match(/00\z/))
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'isbn can not be empty' if isbn.nil? || isbn.empty?
+    raise ArgumentError, 'invalid price'         unless price > 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    '$' + ("%.2f" % price)
+  end
 end
